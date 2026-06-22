4182719fabf1a4e79e3632834a6314a233c1708d
Hina Ayub <hinaayub2002@gmail.com>
2026-06-23 00:32:19 +0500

    3 routes and shared layout

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Task from "./pages/Task";
+import Layout from "./components/Layout";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/tasks" element={<Task />} />
+        <Route path="/about" element={<About />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
